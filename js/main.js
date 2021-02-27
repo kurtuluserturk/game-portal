3f01497cfe1d4f6990ad566742718ba19311c299
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-02-27 20:53:53 +0300

    responsive design
    Responsive design added

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/* CUSTOM OWL CAROUSEL */
+
+$(document).ready(function () {
+    let owl = $(".owl-carousel").owlCarousel({
+        autoplay: false,
+        dots: true,
+        dotsData: true,
+        loop: true,
+        nav: false,
+        items: 1
+    });
+
+    $('.owl-dot').click(function () {
+        owl.trigger('to.owl.carousel', [$(this).index(), 1000]);
+    })
+});
+
+/* OPEN - CLOSE MENU */
+
+$('#check').change(function () {
+    if ($(this).is(':checked')) {
+        $('.nav-header').addClass('active');
+    } else {
+        $('.nav-header').removeClass('active');
+    }
+});
